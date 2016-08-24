3176bbc87005881f43678abfff4148f449d0e70d
Kinshuk Saxena <ksstudyemail@gmail.com>
2016-08-24 12:11:15 -0400

    Initialize repo

---
+++ server.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var app = express();
+
+var middleware = {
+	requireAuthentication: function (req, res, next){
+		console.log('Private route hit!');
+		next();
+	},
+	logger: function (req, res, next) {
+
+		console.log('Request: ' + new Date().toString() + ' ' + req.method + ' ' + req.originalUrl);
+		next();
+	}
+};
+
+app.use(middleware.logger);
+
+// app.get('/', function (req, res) {
+//   res.send('Hello Express!');
+// });
+
+// /about
+app.get('/about', middleware.requireAuthentication, function (req, res) {
+	res.send('About us');
+});
+
+// expose an entire folder
+app.use(express.static(__dirname + '/public'));
+
+app.listen(3000, function (){
+	console.log('Express server started!');
+});
